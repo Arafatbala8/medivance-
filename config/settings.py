52d43eac393c7141f29fa95511fde48f5a9202d4
arafatbala8 <arafatgaladima33@gmail.com>
2026-02-06 08:56:09 +0200

    Update product image handling and UI tweaks

--- config/settings.py
+++ config/settings.py
@@ -97,3 +97,14 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 WHATSAPP_NUMBER = "2348074000598"
+
+INSTALLED_APPS += ["cloudinary", "cloudinary_storage"]
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+}
+
